353b20696c4d25909cd2e0281246bae0b89896e8
SebastianUVG <156974025+SebastianUVG@users.noreply.github.com>
2024-02-12 23:22:49 -0600

    Delete Cosas del lisp directory

--- Cosas del lisp/conversor.lisp
+++
@@ -1,6 +0,0 @@
-(defun fahrenheit-a-celsius (fahrenheit)
-  "Convierte grados Fahrenheit a grados Celsius."
-  (/ (* (- fahrenheit 32) 5) 9))
-
-(let ((grados-fahrenheit 33)) ; Puedes cambiar el valor según tus necesidades
-  (format t "Fahrenheit: ~a ~%Celsius: ~a" grados-fahrenheit (fahrenheit-a-celsius grados-fahrenheit)))
