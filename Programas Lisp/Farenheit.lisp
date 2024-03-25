c70d37307507a39b8798df0745d0c501a3dcf4dc
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-03-24 23:38:10 -0600

    Ultimos documentos

--- Programas Lisp/Farenheit.lisp
+++
@@ -1,8 +0,0 @@
-(defun celsius-a-fahrenheit (celsius)
-  "Convierte grados Celsius a grados Fahrenheit"
-  (+ (* celsius 9/5) 32))
-
-;; Ejemplo de uso:
-(let ((temperatura-celsius 25))
-  (format t "Temperatura en Celsius: ~a~%" temperatura-celsius)
-  (format t "Temperatura en Fahrenheit: ~a~%" (celsius-a-fahrenheit temperatura-celsius)))
